cfa074fcdd3a1cfa28771000876352c33f3df033
madushanrathnayakasl <jaliyamadushan21@gmail.com>
2024-09-16 23:24:45 +0530

    《MADUSHAN🔰RATHNAYAKA》

--- plugins/Menu.js
+++ plugins/Menu.js
@@ -1,109 +1,104 @@
-let DataPack = require('sew-queen-pro');
+const {cmd , commands} = require('../command')
 
-let SewQueen = require('sew-queen-pro/sources/dc/handler');
-let Details = require('sew-queen-pro/sources/dc/Details');
-let {sendMessageAllMenupv,sendMessageAllMenupb, sendMessageonemenupv,sendMessageonemenupb } = require('sew-queen-pro/sources/dc/cmd/menu')
-let WorkType = Details.WORKTYPE == 'public' ? false : true
-SewQueen['IntroduceCMD']({
-        pattern: 'help ?(.*)', 
-        fromMe: false, 
-        dontAdCommandList: true,
-        }, 
-(async (MADUSHAN-MD,input) => { 
- if(Details.WORKTYPE !== 'public') return;
-await sendMessageAllMenupb(MADUSHAN-MD,input)
-})); 
-MADUSHAN-MD['IntroduceCMD']({
-        pattern: 'help ?(.*)', 
-        fromMe: true, 
-        dontAdCommandList: true,
-        }, 
-(async (QueenSew,input) => { 
-await sendMessageAllMenupv(QueenSew,input)
-})); 
-SewQueen['IntroduceCMD']({
-        pattern: 'list ?(.*)', 
-        fromMe: false, 
-        dontAdCommandList: true,
-        }, 
-(async (QueenSew,input) => { 
- if(Details.WORKTYPE !== 'public') return;
-await sendMessageAllMenupb(QueenSew,input)
-})); 
-SewQueen['IntroduceCMD']({
-        pattern: 'list ?(.*)', 
-        fromMe: true, 
-        dontAdCommandList: true,
-        }, 
-(async (QueenSew,input) => { 
-await sendMessageAllMenupv(QueenSew,input)
-})); 
-SewQueen['IntroduceCMD']({
-        pattern: 'cmd ?(.*)', 
-        fromMe: false, 
-        dontAdCommandList: true,
-        }, 
-(async (QueenSew,input) => { 
- if(Details.WORKTYPE !== 'public') return;
-await sendMessageAllMenupb(QueenSew,input)
-})); 
-SewQueen['IntroduceCMD']({
-        pattern: 'cmd ?(.*)', 
-        fromMe: true, 
-        dontAdCommandList: true,
-        }, 
-(async (QueenSew,input) => { 
-await sendMessageAllMenupv(QueenSew,input)
-})); 
-SewQueen['IntroduceCMD']({
-        pattern: 'මෙනු ?(.*)', 
-        fromMe: false, 
-        dontAdCommandList: true,
-        }, 
-(async (QueenSew,input) => { 
- if(Details.WORKTYPE !== 'public') return;
-await sendMessageAllMenupb(QueenSew,input)
-})); 
-SewQueen['IntroduceCMD']({
-        pattern: 'මෙනු ?(.*)', 
-        fromMe: true, 
-        dontAdCommandList: true,
-        }, 
-(async (QueenSew,input) => { 
-await sendMessageAllMenupv(QueenSew,input)
-})); 
-SewQueen['IntroduceCMD']({
-        pattern: 'menu ?(.*)', 
-        fromMe: false, 
-        dontAdCommandList: true,
-        }, 
-(async (QueenSew,input) => { 
- if(Details.WORKTYPE !== 'public') return;
-await sendMessageAllMenupb(QueenSew,input)
-})); 
-SewQueen['IntroduceCMD']({
-        pattern: 'menu ?(.*)', 
-        fromMe: true, 
-        dontAdCommandList: true,
-        }, 
-(async (QueenSew,input) => { 
-await sendMessageAllMenupv(QueenSew,input)
-})); 
-SewQueen['IntroduceCMD']({
-        pattern: 'letcmd ?(.*)', 
-        fromMe: false, 
-        dontAdCommandList: true,
-        }, 
-(async (QueenSew,input) => { 
- if(Details.WORKTYPE !== 'public') return;
-await sendMessageonemenupb(QueenSew,input)
-})); 
+cmd({
+    pattern: "menu",
+    react: "📜",
+    alias: ["panel"],
+    desc: "Get bot\'s command list.",
+    category: "main",
+    use: '.menu',
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
 
-SewQueen['IntroduceCMD']({
-        pattern: 'letcmd ?(.*)', 
-        fromMe: true, 
-        dontAdCommandList: true,
-        }, 
-(async (QueenSew,input) => { 
-await sendMessageonemenupv(QueenSew,input)
-})); 
+let tex = `
+*👋 Hello ${pushname}*
+╭───────────◉◉►
+│Hey, I'm 𝐌𝐀𝐃𝐔𝐒𝐇𝐀𝐍-𝐌𝐃, Created By
+│𝐉𝐀𝐋𝐈𝐘𝐀 𝐌𝐀𝐃𝐔𝐒𝐇𝐀𝐍✨🍂
+│
+│I'm always here for help you.😇 Love │you forever ♥
+╰───────────◉◉►
+
+╭───────────◎◎►
+> ☟𝐃𝐎𝐖𝐍𝐋𝐎𝐃𝐄 𝐂𝐎𝐌𝐌𝐀𝐍𝐃☟
+> ─────────*
+> ►.song*
+> ►.video*
+> ►.fb*
+> ►.tt*
+> ►.movie*
+> ►.mediafire*
+> ►.twitter*
+> ►.gdrive*
+> ►.apk*
+> ►.img*
+> ►.rvideo* 
+> ─────────
+> ☟𝐒𝐄𝐀𝐑𝐂𝐇 𝐂𝐎𝐌𝐌𝐀𝐍𝐃𝐒☟
+> ─────────*
+> ►.yts*
+> ►.news*
+> ►.githubstalk*
+> ►.srepo*
+> ►.weather*
+> ─────────
+> ☟𝐀𝐈 𝐂𝐎𝐌𝐌𝐀𝐍𝐃𝐒☟
+> ─────────*
+> ►.ai*
+> ─────────
+> ☟𝐂𝐎𝐍𝐕𝐄𝐑𝐓 𝐂𝐎𝐌𝐌𝐀𝐍𝐃𝐒☟
+> ─────────*
+> ►.convert*
+> ►.trt*
+> ─────────
+> ☯𝐎𝐖𝐄𝐍𝐄𝐑 𝐂𝐎𝐌𝐌𝐀𝐍𝐃𝐒☯
+> ─────────*
+> ►.restart*
+> ─────────
+> ☟𝐌𝐀𝐈𝐍 𝐂𝐎𝐌𝐌𝐀𝐍𝐃𝐒☟
+> ─────────*
+> ►.menu*
+> ►.alive*
+> ►.ping*
+> ►.repo*
+> ►.owner*
+> ►.about*
+> ►.support*
+> ─────────
+> ☟𝐎𝐓𝐇𝐄𝐑 𝐂𝐎𝐌𝐌𝐀𝐍𝐃𝐒☟
+> ─────────*
+> ►.kylie*
+> ►.hack*
+> ►.define*
+> ►.gpass*
+> ─────────
+> ☟𝐅𝐔𝐍 𝐂𝐎𝐌𝐌𝐀𝐍𝐃𝐒☟
+> ─────────*
+> ►.animegirl*
+> ►.dog*
+> ►.fact*
+> ►.joke*
+> ►.quote*
+> ─────────
+> ☟𝐆𝐑𝐎𝐔𝐏 𝐂𝐎𝐌𝐌𝐀𝐍𝐃𝐒☟
+> ─────────
+> ►.promote
+> ►.demote
+> ►.remove
+> ►.setpic
+> ►.del*
+> ►.add*
+> ►.setwelcome*
+> ►.setgoodbye*
+╰───────────◎◎►
+> ᴘᴏᴡᴇʀᴇᴅ ʙʏ 𝙼𝙰𝙳𝚄𝚂𝙷𝙰𝙽 𝙼𝙳
+`
+
+return await conn.sendMessage(from,{image: {url: `https://i.ibb.co/jWbPWSR/Whats-App-Image-2024-09-16-at-21-05-11-d95b643c.jpg`},caption: tex},{quoted: mek})
+}catch(e){
+console.log(e)
+reply(`${e}`)
+}
+})
